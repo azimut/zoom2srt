920633362568024e788931e96bc86fd1635d989f
azimut <azimut.github@protonmail.com>
2024-12-01 20:15:38 -0300

    style: add 20 nick colors

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # zoom2srt
 
-Online (no server!) tool to convert Zoom exported chats (in .txt) to srt subtitles that you can add to your favorite video player.
+Online (no backend!) tool to convert Zoom exported chats (in .txt) to srt subtitles that you can add to your favorite video player.
 
 ## References
 
 - [my previous version of zoom2srt, made in AWK](https://gist.github.com/azimut/bad34fd818b179ef5e78acd8c3b8811d)
 - ["How to create a file in memory for user to download, but not through server?"](https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server)
 - [CSS: Rainbow text](https://w3bits.com/rainbow-text/)
+- [20 distinct colors](https://sashamaps.net/docs/resources/20-colors/)
--- public/script.js
+++ public/script.js
@@ -1,8 +1,10 @@
-function processChat(chat, { duration, offset, nickStyle }) {
+function processChat(chat, opts) {
+  const { duration, offset, nickStyle } = opts;
   return (
     formatIt(
       offsetIt(
-        parseIt(chat, duration),
+        parseIt(chat,
+          duration),
         offset),
       nickStyle));
 }
@@ -51,7 +53,7 @@ function offsetIt(messages, rawOffset) {
 function formatIt(messages, nickStyle) {
   let iColor = 0
   let colorMap = new Map()
-  const colors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red"]
+  const colors = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4', '#42d4f4', '#f032e6', '#bfef45', '#fabed4', '#469990', '#dcbeff', '#9A6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#ffffff', '#000000'];
   const getColor = (nick) => {
     if (!colorMap.get(nick)) colorMap.set(nick, colors[iColor++ % colors.length])
     return colorMap.get(nick)
@@ -70,8 +72,8 @@ function formatIt(messages, nickStyle) {
     result += '\n'
   }
   return result
-  function toTimestamp(posix) {
-    const date = new Date(posix * 1000) // to milliseconds
+  function toTimestamp(seconds) {
+    const date = new Date(seconds * 1000) // to milliseconds
     return `${pad2(date.getUTCHours())}:${pad2(date.getUTCMinutes())}:${pad2(date.getUTCSeconds())}`
     function pad2(s) {
       return s.toString().padStart(2, '0')
